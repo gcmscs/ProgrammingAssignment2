3d0579e85c436d577b19801e8fbae7ad50143a38
gcmscs <gcmscs@users.noreply.github.com>
2015-03-18 19:24:28 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,52 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+    # x = original matrix
+    # m = inverted matrix
+    m  <- NULL
+    set  <- function(y)
+    {
+        x <<- y
+        m <<- NULL 
+    }
+    
+    # return matrix
+    get  <- function() x
+    
+    # override previous inverse and assign argument to inverse
+    setinverse  <- function(inverse) m  <<- inverse
+    
+    #return inverse
+    getinverse  <- function() m
+    
+    # create list of function
+    list(set= set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+    # get inverse
+    m  <- x$getinverse()
+    
+    # if inverse exists, return cached inverse
+    if (!is.null(m))
+    {
+      message("getting cached data")
+      return(m)
+    }
+    
+    #get matrix
+    data  <- x$get()
+    
+    # inverse computation
+    m  <- solve(data, ...)
+    
+    # caching inverse
+    x$setinverse(m)
+    
+    #return inverse
+    m
 }
